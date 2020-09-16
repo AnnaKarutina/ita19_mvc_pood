c9bce9722fa9a6157d9cf9593125174ee777ee5e
Anna Karutina <anna.karutina@khk.ee>
2020-09-16 11:47:00 +0300

    Described structure of MVC

--- ui.py
+++ ui.py
@@ -3,6 +3,9 @@
 # import classes and files
 from product import Product
 from shop import Shop
+from controller import Controller
+from model import Model
+from view import View
 
 # create products
 bread = Product("bread", 0.80, 10)
@@ -10,9 +13,7 @@ milk = Product("milk", 0.50, 50)
 wine = Product("wine", 5.60, 5)
 
 # create shop and add products to shop
-shop = Shop()
-shop.addProduct(bread)
-shop.addProduct(milk)
-shop.addProduct(wine)
-# test shop content
-print(shop)
+shop = Controller(Model(Shop()), View())
+shop.addItem("bread", 0.80, 10)
+shop.addItem("milk", 0.50, 50)
+shop.addItem("wine", 5.60, 5)
